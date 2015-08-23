ef6cfd4ef4e7252861b3bb4827959d28a9a1d16b
Jade Shan <evokeralucard@gmail.com>
2015-08-23 22:10:52 +0800

    use json4s to process json

--- crawler-web2/src/main/scala/webapi/apis.scala
+++ crawler-web2/src/main/scala/webapi/apis.scala
@@ -2,12 +2,13 @@ package jadecrawler.web2
 
 import org.slf4j.LoggerFactory
 
+import org.json4s._
+import org.json4s.JsonDSL._
+import org.json4s.jackson.JsonMethods._
+
 import jadeutils.web.BasicController
 import jadeutils.web.DispatherInfo.paramsToString
 import jadeutils.web.DispatherServlet
-
-import jadeutils.common.JsonArray.JArr
-import jadeutils.common.JsonObject.JObj
 
 import jadecrawler.website.IcibaCrawler
 
@@ -32,39 +33,34 @@ class IcibaApiController extends BasicController {
 		IcibaCrawler.addNewWord(dbHost, dbPort, info.params("username")(0),
 			info.params("password")(0), info.params("word")(0))
 
-		info.response.setContentType("application/json");
-		info.response.setHeader("Content-disposition", "inline");
-		val out = info.response.getWriter()
-		val result = JObj ~ ("status", "success")
-		out.println(result.toString)
+		val result = ("status" -> "success"): JValue
+
+		info.response.setContentType("application/json")
+		info.response.setHeader("Content-disposition", "inline")
+		info.response.getWriter.println(compact(render(result)))
 	}}
 
 	service("/api/dictionary/removenewword/${word}") { (info) => {
 		IcibaCrawler.removeNewWord(dbHost, dbPort, info.params("username")(0),
 			info.params("password")(0), info.params("word")(0))
 
-		info.response.setContentType("application/json");
-		info.response.setHeader("Content-disposition", "inline");
-		val out = info.response.getWriter()
-		val result = JObj ~ ("status", "success")
-		out.println(result.toString)
+		val result = ("status" -> "success"): JValue
+
+		info.response.setContentType("application/json")
+		info.response.setHeader("Content-disposition", "inline")
+		info.response.getWriter.println(compact(render(result)))
 	}}
 
 	service("/api/dictionary/newwords/") { (info) => {
 		val ll = IcibaCrawler.loadNewWords(dbHost, dbPort,
 			info.params("username")(0), info.params("password")(0))
-		logger.debug(ll.toString)
 
-		val words = JArr
-		logger.debug(words.toString)
-		for (i <- 0 until ll.size) words ~~ (JObj ~ ("word", ll.get(i).word))
-			logger.debug(words.toString)
+		val result = ("result" -> (for (i <- 0 until ll.size) yield ll.get(i)).map(
+			w => ("word" -> w.word))): JValue
 
-		info.response.setContentType("application/json");
-		info.response.setHeader("Content-disposition", "inline");
-		val out = info.response.getWriter()
-		val result = JObj ~ ("result", words)
-		out.println(result.toString)
+		info.response.setContentType("application/json")
+		info.response.setHeader("Content-disposition", "inline")
+		info.response.getWriter.println(compact(render(result)))
 	}}
 
 	service("/api/dictionary/eng2chs/${word}") { (info) => {
@@ -77,47 +73,31 @@ class IcibaApiController extends BasicController {
 			rec
 		}
 
-		val pronunciations = JArr
-		for (i <- 0 until data.pronunciations.size) pronunciations ~~ (JObj ~ 
-			("str1", data.pronunciations.get(i).str1) ~ 
-			("str2", data.pronunciations.get(i).str2) ~ 
-			("str3", data.pronunciations.get(i).str3))
+		val result = ("result" -> 
+			("word" -> data.word) ~ 
+			("pronunciations" -> (for (i <- 0 until data.pronunciations.size) yield 
+				data.pronunciations.get(i)).map(p => 
+				(("str1" -> p.str1) ~ ("str2" -> p.str2) ~ ("str3" -> p.str3)))) ~ 
+			("explantions" -> (for (i <- 0 until data.explantions.size) yield 
+				data.explantions.get(i)).map(p => 
+				(("str1" -> p.str1) ~ ("str2" -> p.str2)))) ~ 
+			("relatedWords" -> (for (i <- 0 until data.relatedWords.size) yield 
+				data.relatedWords.get(i)).map(p => 
+				(("str1" -> p.str1) ~ ("str2" -> p.str2)))) ~ 
+			("examples" -> (for (i <- 0 until data.examples.size) yield 
+				data.examples.get(i)).map(p => 
+				(("str1" -> p.str1) ~ ("str2" -> p.str2) ~ ("str3" -> p.str3)))) ~ 
+			("homoionyms" -> (for (i <- 0 until data.homoionyms.size) yield 
+				data.homoionyms.get(i)).map(p => 
+				(("str" -> p.str) ~ ("s2dto" -> (for (i <- 0 until p.s2dto.size) yield 
+					p.s2dto.get(i)).map( d => (("str1" -> d.str1) ~ ("str2" -> d.str2)))))))
+			): JValue
 
-		val explantions = JArr
-		for (i <- 0 until data.explantions.size) explantions ~~ (JObj ~ 
-			("str1", data.explantions.get(i).str1) ~ 
-			("str2", data.explantions.get(i).str2))
-
-		val relatedWords = JArr
-		for (i <- 0 until data.relatedWords.size) relatedWords ~~ (JObj ~ 
-			("str1", data.relatedWords.get(i).str1) ~ 
-			("str2", data.relatedWords.get(i).str2))
-
-		val examples = JArr
-		for (i <- 0 until data.examples.size) examples ~~ (JObj ~ 
-			("str1", data.examples.get(i).str1) ~ 
-			("str2", data.examples.get(i).str2) ~ 
-			("str3", data.examples.get(i).str3))
-
-		val homoionyms = JArr
-		for (i <- 0 until data.homoionyms.size) {
-			val s2dto = JArr
-			for (j <- 0 until data.homoionyms.get(i).s2dto.size) s2dto ~~ (JObj ~ 
-				("str1", data.homoionyms.get(i).s2dto.get(j).str1) ~ 
-				("str2", data.homoionyms.get(i).s2dto.get(j).str2))
-			homoionyms ~~ (JObj ~ ("str", data.homoionyms.get(i).str) ~ 
-				("s2dto", s2dto))
+			info.response.setContentType("application/json")
+			info.response.setHeader("Content-disposition", "inline")
+			info.response.getWriter.println(compact(render(result)))
 		}
-
-		info.response.setContentType("application/json");
-		info.response.setHeader("Content-disposition", "inline");
-		val out = info.response.getWriter()
-		val result = JObj ~ ("result", JObj ~ 
-			("word", word) ~ ("pronunciations", pronunciations) ~
-			("explantions", explantions) ~ ("relatedWords", relatedWords) ~
-			("examples", examples) ~ ("homoionyms", homoionyms))
-		out.println(result.toString)
-	}}
+	}
 
 }
 
@@ -126,19 +106,19 @@ object IcibaApiController {
 }
 
 /*
-//scripts import word list
+ //scripts import word list
 
-import jadecrawler.website.IcibaCrawler
-import scala.io.Source
+ import jadecrawler.website.IcibaCrawler
+ import scala.io.Source
 
-val dao = IcibaCrawler.getDao(dbHost, dbPort)
+ val dao = IcibaCrawler.getDao(dbHost, dbPort)
 
-def transWord(word: String) {
-if (null == IcibaCrawler.loadLocal(dao, word)) { 
-val rec = jadecrawler.website.IcibaCrawler.process(word)
-if (null != rec) IcibaCrawler.saveLocal(dao, rec)
-}
-}
+ def transWord(word: String) {
+	 if (null == IcibaCrawler.loadLocal(dao, word)) { 
+		 val rec = jadecrawler.website.IcibaCrawler.process(word)
+		 if (null != rec) IcibaCrawler.saveLocal(dao, rec)
+		 }
+ }
 
-scala.io.Source.fromFile("/home/morgan/CET-4.txt").getLines.foreach((s) =>{transWord(s);Thread.sleep(200)})
-*/
+ scala.io.Source.fromFile("/home/morgan/CET-4.txt").getLines.foreach((s) =>{transWord(s);Thread.sleep(200)})
+ */
