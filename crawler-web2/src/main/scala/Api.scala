a758de4c5b27971475846173618d013590d0bd2a
Jade Shan <evokeralucard@gmail.com>
2015-09-17 14:59:42 +0800

    refactor iciba app

--- crawler-web2/src/main/scala/Api.scala
+++ crawler-web2/src/main/scala/Api.scala
@@ -1,8 +1,8 @@
 package jadecrawler.web2
 
-
+import jadeutils.web.BasicController
 
 class ApiDispather extends jadeutils.web.DispatherServlet 
 { ApiDispather.controllers = new IcibaApiController :: Nil }
 
-object ApiDispather { var controllers: List[IcibaApiController] = Nil }
+object ApiDispather { var controllers: List[BasicController] = Nil }
