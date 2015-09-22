817ac09ad8e5f7750a646d81c2fea4f82aefe86a
Jade Shan <evokeralucard@gmail.com>
2015-09-23 00:19:42 +0800

    add auth to record action

--- crawler-web2/src/main/scala/workout/workoutRecController.scala
+++ crawler-web2/src/main/scala/workout/workoutRecController.scala
@@ -7,37 +7,45 @@ import org.json4s.jackson.JsonMethods._
 import jadeutils.common.Logging
 
 import jadeutils.web.BasicController
-import jadeutils.web.Method._
+import jadeutils.web.DispatherInfo
 import jadeutils.web.Foward
+import jadeutils.web.Method._
 import jadeutils.web.Redirect
 
 trait BaseWorkoutController extends BasicController with WorkoutAppCtx
 
 object WorkoutAuthController extends BaseWorkoutController with Logging
 {
-	service("/api/workout/user/auth") {(info) => {
-		try {
-			val auth = decodeHttpBasicAuth(info.request.getHeader("Authorization"))
-			logger.debug("after auth check: {}", auth)
-			val username = auth._2
-			val password = auth._3
-			if (auth._1 && null != username && username.trim.length > 0) {
-				val rec = RecDaos.userAuthDao.findAuth(username)
-				logger.debug(rec.toString)
-				if (rec.size > 0 && password == rec(0).password) {
-					("status" -> "success") ~ ("auth" -> "success"): JValue
-				} else {
-					("status" -> "success") ~ ("auth" -> "fail") ~
-					("reason" -> "not match"): JValue
-				}
-			} else {
-				("status" -> "success") ~ ("auth" -> "fail") ~
-				("reason" -> "no username"): JValue
-			}
+	/**
+	 * (isAuth, isSuccess)
+	 */
+	def auth(info: DispatherInfo): (Boolean, Boolean) = {
+		val auth = decodeHttpBasicAuth(info.request.getHeader("Authorization"))
+		logger.debug("after auth check: {}", auth)
+		val username = auth._2
+		val password = auth._3
+		if (auth._1 && null != username && username.trim.length > 0) try {
+			val rec = RecDaos.userAuthDao.findAuth(username)
+			logger.debug(rec.toString)
+			if (rec.size > 0 && password == rec(0).password) {
+				(true, true)
+			} else (true, false)
 		} catch {
 			case e: Exception => {
 				logger.error(e.toString)
-				("status" -> "error") ~ ("err" -> e.toString): JValue
+				(true, false)
+			}
+		} else (false, false)
+	}
+
+	service("/api/workout/user/auth") {(info) => {
+		auth(info) match {
+			case (true, true) => {
+				("status" -> "success") ~ ("auth" -> "success"): JValue
+			}
+			case _ => {
+				("status" -> "success") ~ ("auth" -> "fail") ~ 
+				("reason" -> "not match"): JValue
 			}
 		}
 	}}
@@ -68,41 +76,47 @@ object WorkoutRecController extends BaseWorkoutController with Logging
 	}}
 
 	service("/api/workout/recordAerobicRec") {(info) => {
-		try {
-			val user     = info.params("username")(0)
-			val item     = info.params("workoutId")(0)
-			val time     = Integer.parseInt(info.params("time")(0))
-			val distance = Integer.parseInt(info.params("distance")(0))
-			val calories = Integer.parseInt(info.params("calories")(0))
-			val rec = new AerobicRecord(user, item, time, distance, calories, 
-				System.currentTimeMillis)
-			logger.debug(rec.toString)
-			RecDaos.aerobicRecordDao.insert(rec)
-			("status" -> "success"): JValue
-		} catch {
-			case e: Exception => {
-				logger.error(e.toString)
-				("status" -> "error") ~ ("err" -> e.toString): JValue
+		WorkoutAuthController.auth(info) match {
+			case (true, true) => try {
+				val user     = info.params("username")(0)
+				val item     = info.params("workoutId")(0)
+				val time     = Integer.parseInt(info.params("time")(0))
+				val distance = Integer.parseInt(info.params("distance")(0))
+				val calories = Integer.parseInt(info.params("calories")(0))
+				val rec = new AerobicRecord(user, item, time, distance, calories, 
+					System.currentTimeMillis)
+				logger.debug(rec.toString)
+				RecDaos.aerobicRecordDao.insert(rec)
+				("status" -> "success"): JValue
+			} catch {
+				case e: Exception => {
+					logger.error(e.toString)
+					("status" -> "error") ~ ("err" -> e.toString): JValue
+				}
 			}
+			case _ => ("status" -> "error") ~ ("err" -> "need login"): JValue
 		}
 	}}
 
 	service("/api/workout/recordStrengthRec") {(info) => {
-		try {
-			val user    = info.params("username")(0)
-			val item    = info.params("workoutId")(0)
-			val weight  = Integer.parseInt(info.params("weight")(0))
-			val repeat  = Integer.parseInt(info.params("repeat")(0))
-			val rec = new StrengthRecord(user, item, weight, repeat, 
-				System.currentTimeMillis)
-			logger.debug(rec.toString)
-			RecDaos.strengthRecordDao.insert(rec)
-			("status" -> "success"): JValue
-		} catch {
-			case e: Exception => {
-				logger.error(e.toString)
-				("status" -> "error") ~ ("err" -> e.toString): JValue
+		WorkoutAuthController.auth(info) match {
+			case (true, true) => try {
+				val user    = info.params("username")(0)
+				val item    = info.params("workoutId")(0)
+				val weight  = Integer.parseInt(info.params("weight")(0))
+				val repeat  = Integer.parseInt(info.params("repeat")(0))
+				val rec = new StrengthRecord(user, item, weight, repeat, 
+					System.currentTimeMillis)
+				logger.debug(rec.toString)
+				RecDaos.strengthRecordDao.insert(rec)
+				("status" -> "success"): JValue
+			} catch {
+				case e: Exception => {
+					logger.error(e.toString)
+					("status" -> "error") ~ ("err" -> e.toString): JValue
+				}
 			}
+			case _ => ("status" -> "error") ~ ("err" -> "need login"): JValue
 		}
 	}}
 	
