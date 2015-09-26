9fe157580cf7b5f0295bdb542811d71911c70c41
Jade Shan <evokeralucard@gmail.com>
2015-09-27 01:45:00 +0800

    update

---
+++ crawler-web2/src/main/webapp/static/images/workout/aa.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+cp unknow.svg sth-2-4.svg
+cp unknow.svg sth-2-5.svg
+cp unknow.svg sth-2-6.svg
+cp unknow.svg sth-2-7.svg
+cp unknow.svg sth-2-8.svg
+cp unknow.svg sth-2-9.svg
+cp unknow.svg sth-2-10.svg
+cp unknow.svg sth-2-11.svg
+cp unknow.svg sth-2-12.svg
+cp unknow.svg sth-2-13.svg
+cp unknow.svg sth-2-14.svg
+cp unknow.svg sth-2-15.svg
+cp unknow.svg sth-2-16.svg
+cp unknow.svg sth-2-17.svg
+cp unknow.svg sth-2-18.svg
+cp unknow.svg sth-2-19.svg
+cp unknow.svg sth-2-20.svg
+cp unknow.svg sth-2-21.svg
+cp unknow.svg sth-2-22.svg
+cp unknow.svg sth-2-23.svg
+cp unknow.svg sth-2-24.svg
+cp unknow.svg sth-2-25.svg
+cp unknow.svg sth-2-26.svg
+cp unknow.svg sth-2-27.svg
+cp unknow.svg sth-2-28.svg
+cp unknow.svg sth-2-29.svg
+
