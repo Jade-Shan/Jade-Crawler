3441462cd8f3837d9735cab63a29af1d49d8d890
Jade Shan <evokeralucard@gmail.com>
2015-11-28 21:02:05 +0800

    add 1kk comic crawler

---
+++ crawler-logic/src/test/scala/website/K1Test.scala
@@ -0,0 +1,56 @@
+package jadecrawler.website
+
+import org.slf4j.LoggerFactory
+import org.slf4j.Logger
+
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.FunSuite
+import org.junit.runner.RunWith
+
+import org.json.JSONObject
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
+
+
+import java.util.Properties
+import jadeutils.common.Logging
+
+@RunWith(classOf[JUnitRunner])
+class K1Test extends FunSuite with Logging{
+	import scala.io.Source
+
+	test("Test-scalatest") {
+		val bookStr = K1Crawler.fetchBook("manhua8354")
+		val book = K1Parser.parseBook("manhua8354", bookStr)
+		logInfo("{}", book)
+
+		// val volDatas = for (vol <- book._3) yield {
+		// 	val volData = K1Crawler.fetchVol(vol._1)
+		// 	val cid = if(null == volData) { "no page"} else {
+		// 		val g = """.+ +var +cid *= *(\d+) *;?""".r.findAllIn(volData)
+		// 		if (g.hasNext) g.group(1) else "no find"
+		// 	}
+		// 	val pages = K1Parser.parseVol(vol._1, vol._2, cid, vol._3)
+		// 	val ll = for (p <- pages) yield K1Crawler.fetchPage(p._1, p._3)
+		// 	val urls = for (l <- ll) yield K1Parser.parseImage(l)
+		// 	for(i <- 0 until urls.size) {
+		// 		val url = urls(i)
+		// 		val g = """http://manhua.+\.cdndm5.com.+(\.[0-9a-zA-Z])\?cid=.+""".r.findAllIn(url)
+		// 		val postfix = if (g.hasNext) g.group(1) else ".jpg"
+		// 		K1Crawler.saveImage(vol._1 + "-" + "%03d".format(i + 1) + postfix, K1Crawler.loadImage(url, vol._1))
+		// 	}
+		// 	urls
+		// }
+
+		// K1Crawler.processBook("manhua8354")
+
+
+	}
+
+}
+
+object K1Test { 
+}
+
