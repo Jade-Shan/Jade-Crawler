61934a7194bae3d26f3ca5d114172fbf3e51d6d2
Jade Shan <evokeralucard@gmail.com>
2016-02-03 10:24:00 +0800

    0.1.9-20160202-SNAPSHOT

--- crawler-net/src/main/scala/HTTPCrawler.scala
+++ crawler-net/src/main/scala/HTTPCrawler.scala
@@ -31,13 +31,15 @@ object HTTPUtil {
 	import org.apache.http.HttpEntity
 //	import org.apache.http.impl.client.BasicResponseHandler
 	import org.apache.http.impl.client.CloseableHttpClient
-	import org.apache.http.impl.client.DefaultHttpClient
+//	import org.apache.http.impl.client.DefaultHttpClient
 	import org.apache.http.impl.client.HttpClients
 	import org.apache.http.impl.conn.PoolingHttpClientConnectionManager
 	import org.apache.http.message.BasicNameValuePair
 //	import org.apache.http.NameValuePair
 //	import org.apache.http.protocol.HttpContext
 	import org.apache.http.util.EntityUtils
+
+
 
 	import jadeutils.base.FileOperater
 	import jadeutils.net.FakeDnsResolver
