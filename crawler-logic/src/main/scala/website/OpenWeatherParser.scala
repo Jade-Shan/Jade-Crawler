dc2e56a054dac5f547f5da6b6d37cbcdfd18cce7
Jade Shan <evokeralucard@gmail.com>
2016-03-19 18:51:12 +0800

    update

--- crawler-logic/src/main/scala/website/OpenWeatherParser.scala
+++ crawler-logic/src/main/scala/website/OpenWeatherParser.scala
@@ -37,55 +37,61 @@ object OpenWeatherParser extends Logging {
 		val dow = getDayOfWeek(today("day"), timeZone, local)
 		sb.append(dow).append("\n")
 		// 
-		val smbChar = iconMap.getOrElse(today("smbVar"), 
-			"b")
+		val smbChar = iconMap.getOrElse(today("smbVar"), "b")
 		sb.append(smbChar).append("\n")
 		//
 		sb.append(today("tmpMin")).append(" ~ ").append(today("tmpMax"))
 		sb.append("°C").append("\n")
 		// 
-		sb.append(today("psuVal")).append(today(
-			"psuUnt")).append("\n")
+		sb.append(today("psuVal")).append(today("psuUnt")).append("\n")
 		// 
-		sb.append(today("wndMps")).append(
-			"Mps").append("\n")
-		val wndArw = arrowMap.getOrElse(today(
-			"wndDct"), " ")
+		sb.append(today("wndMps")).append("Mps").append("\n")
+		val wndArw = arrowMap.getOrElse(today("wndDct"), " ")
 		sb.append(wndArw).append("\n")
 
-		val l7 = for (i <- 1 to 4) yield getDayOfWeek(data._5(i)("day"), timeZone, local)
-		sb.append(l7.mkString(
-			"             ")).append("\n")
+		for (i <- 1 to 4) {
+			val str = getDayOfWeek(data._5(i)("day"), timeZone, local)
+			if (i < 4) sb.append("%-20s" format str) else sb.append(str)
+		} 
+		sb.append("\n")
 
-		val l8 = for (i <- 1 to 4) yield iconMap.getOrElse(data._5(i)(
-			"smbVar"), "b")
-		sb.append(l8.mkString(
-			"  ")).append("\n")
+		for (i <- 1 to 4) {
+			val str = iconMap.getOrElse(data._5(i)("smbVar"), "b")
+			if (i < 4) sb.append("%-5s" format str) else sb.append(str)
+		}
+		sb.append("\n")
 
-		val l9 = for (i <- 1 to 4) yield data._5(i)("humVal") +  data._5(i)("humUnt")
-		sb.append(l9.mkString(
-			"  ")).append("\n")
+		for (i <- 1 to 4) {
+			val str = data._5(i)("humVal") +  data._5(i)("humUnt")
+			if (i == 1) sb.append("%8s" format str) else sb.append("%19s" format str)
+		}
+		sb.append("\n")
 
-		val l10 = for (i <- 1 to 4) yield data._5(i)("wndMps") + 
-		"Mps"
-		sb.append(l10.mkString(
-			"         ")).append("\n")
+		for (i <- 1 to 4) {
+			val str = data._5(i)("wndMps") + "Mps"
+			if (i == 1) sb.append("%10s" format str) else sb.append("%15s" format str)
+		}
+		sb.append("\n")
 
-		val l11 = for (i <- 1 to 4) yield arrowMap.getOrElse(data._5(i)("wndDct"), " ")
-		sb.append(l11.mkString("\n")).append("\n")
+		for (i <- 1 to 4) {
+			sb.append(arrowMap.getOrElse(data._5(i)("wndDct"), " ")).append("\n")
+		}
 
-		val l15 = for (i <- 1 to 4) yield data._5(i)("tmpDay") + "°C"
-		sb.append(l15.mkString(
-			"           ")).append("\n")
+		for (i <- 1 to 4) {
+			val str = data._5(i)("tmpMax") + "°C"
+			if (i == 1) sb.append("%9s" format str) else sb.append("%17s" format str)
+		}
+		sb.append("\n")
 
-		sb.append(today(
-			"smbNme")).append("\n")
-		sb.append(today(
-			"humVal")).append(today("humUnt")).append("\n")
+		for (i <- 1 to 4) {
+			val str = data._5(i)("tmpMin") + "°C"
+			if (i == 1) sb.append("%9s" format str) else sb.append("%17s" format str)
+		}
+		sb.append("\n")
+
+		sb.append(today("smbNme")).append("\n")
+		sb.append(today("humVal")).append(today("humUnt")).append("\n")
 		sb.append(data._3).append("\n").append(data._4).append("\n")
-
-
-
 
 		sb.toString
 	}
@@ -185,7 +191,7 @@ object OpenWeatherCrawler extends Logging {
 	val apikey = "b1b15e88fa797225412429c1c50c122a"
 
 	def get16dayByCityName(city: String, localTimeZone: String) = {
-		val page = """<weatherdata><location><name>Shanghai</name><type></type><country>CN</country><timezone></timezone><location altitude="0" latitude="31.222219" longitude="121.458061" geobase="geonames" geobaseid="1796236"></location></location><credit></credit><meta><lastupdate></lastupdate><calctime>0.0266</calctime><nextupdate></nextupdate></meta><sun rise="2016-03-17T21:59:38" set="2016-03-18T10:04:46"></sun><forecast><time day="2016-03-18"><symbol number="500" name="light rain" var="10n"></symbol><precipitation></precipitation><windDirection deg="56" code="NE" name="NorthEast"></windDirection><windSpeed mps="2.86" name="Light breeze"></windSpeed><temperature day="15" min="15" max="15" night="15" eve="15" morn="15"></temperature><pressure unit="hPa" value="1029.04"></pressure><humidity value="93" unit="%"></humidity><clouds value="clear sky" all="0" unit="%"></clouds></time><time day="2016-03-19"><symbol number="500" name="light rain" var="10d"></symbol><precipitation value="0.23" type="rain"></precipitation><windDirection deg="47" code="NE" name="NorthEast"></windDirection><windSpeed mps="3.77" name="Gentle Breeze"></windSpeed><temperature day="15.76" min="6.89" max="18.86" night="6.89" eve="17.12" morn="12.26"></temperature><pressure unit="hPa" value="1032.9"></pressure><humidity value="79" unit="%"></humidity><clouds value="clear sky" all="8" unit="%"></clouds></time><time day="2016-03-20"><symbol number="800" name="clear sky" var="02d"></symbol><precipitation></precipitation><windDirection deg="65" code="ENE" name="East-northeast"></windDirection><windSpeed mps="4.71" name="Gentle Breeze"></windSpeed><temperature day="15.29" min="3.6" max="17.08" night="7.55" eve="14.23" morn="3.6"></temperature><pressure unit="hPa" value="1036.45"></pressure><humidity value="59" unit="%"></humidity><clouds value="clear sky" all="8" unit="%"></clouds></time><time day="2016-03-21"><symbol number="502" name="heavy intensity rain" var="10d"></symbol><precipitation value="15.16" type="rain"></precipitation><windDirection deg="115" code="ESE" name="East-southeast"></windDirection><windSpeed mps="6.62" name="Moderate breeze"></windSpeed><temperature day="12.79" min="11.07" max="12.79" night="12.12" eve="11.65" morn="11.07"></temperature><pressure unit="hPa" value="1030.19"></pressure><humidity value="0" unit="%"></humidity><clouds value="overcast clouds" all="100" unit="%"></clouds></time><time day="2016-03-22"><symbol number="501" name="moderate rain" var="10d"></symbol><precipitation value="11.99" type="rain"></precipitation><windDirection deg="84" code="E" name="East"></windDirection><windSpeed mps="6.84" name="Moderate breeze"></windSpeed><temperature day="15.99" min="11.54" max="15.99" night="11.54" eve="12.55" morn="13.37"></temperature><pressure unit="hPa" value="1023.35"></pressure><humidity value="0" unit="%"></humidity><clouds value="overcast clouds" all="97" unit="%"></clouds></time><time day="2016-03-23"><symbol number="501" name="moderate rain" var="10d"></symbol><precipitation value="10.75" type="rain"></precipitation><windDirection deg="18" code="NNE" name="North-northeast"></windDirection><windSpeed mps="5.51" name="Moderate breeze"></windSpeed><temperature day="11.39" min="8.22" max="11.39" night="8.22" eve="9.62" morn="10.57"></temperature><pressure unit="hPa" value="1029.89"></pressure><humidity value="0" unit="%"></humidity><clouds value="overcast clouds" all="97" unit="%"></clouds></time><time day="2016-03-24"><symbol number="500" name="light rain" var="10d"></symbol><precipitation value="1.67" type="rain"></precipitation><windDirection deg="25" code="NNE" name="North-northeast"></windDirection><windSpeed mps="7.47" name="Moderate breeze"></windSpeed><temperature day="13.35" min="6.82" max="13.35" night="6.82" eve="7.55" morn="8.62"></temperature><pressure unit="hPa" value="1036.05"></pressure><humidity value="0" unit="%"></humidity><clouds value="few clouds" all="24" unit="%"></clouds></time></forecast></weatherdata>"""
+		// val page = """<weatherdata><location><name>Shanghai</name><type></type><country>CN</country><timezone></timezone><location altitude="0" latitude="31.222219" longitude="121.458061" geobase="geonames" geobaseid="1796236"></location></location><credit></credit><meta><lastupdate></lastupdate><calctime>0.0266</calctime><nextupdate></nextupdate></meta><sun rise="2016-03-17T21:59:38" set="2016-03-18T10:04:46"></sun><forecast><time day="2016-03-18"><symbol number="500" name="light rain" var="10n"></symbol><precipitation></precipitation><windDirection deg="56" code="NE" name="NorthEast"></windDirection><windSpeed mps="2.86" name="Light breeze"></windSpeed><temperature day="15" min="15" max="15" night="15" eve="15" morn="15"></temperature><pressure unit="hPa" value="1029.04"></pressure><humidity value="93" unit="%"></humidity><clouds value="clear sky" all="0" unit="%"></clouds></time><time day="2016-03-19"><symbol number="500" name="light rain" var="10d"></symbol><precipitation value="0.23" type="rain"></precipitation><windDirection deg="47" code="NE" name="NorthEast"></windDirection><windSpeed mps="3.77" name="Gentle Breeze"></windSpeed><temperature day="15.76" min="6.89" max="18.86" night="6.89" eve="17.12" morn="12.26"></temperature><pressure unit="hPa" value="1032.9"></pressure><humidity value="79" unit="%"></humidity><clouds value="clear sky" all="8" unit="%"></clouds></time><time day="2016-03-20"><symbol number="800" name="clear sky" var="02d"></symbol><precipitation></precipitation><windDirection deg="65" code="ENE" name="East-northeast"></windDirection><windSpeed mps="4.71" name="Gentle Breeze"></windSpeed><temperature day="15.29" min="3.6" max="17.08" night="7.55" eve="14.23" morn="3.6"></temperature><pressure unit="hPa" value="1036.45"></pressure><humidity value="59" unit="%"></humidity><clouds value="clear sky" all="8" unit="%"></clouds></time><time day="2016-03-21"><symbol number="502" name="heavy intensity rain" var="10d"></symbol><precipitation value="15.16" type="rain"></precipitation><windDirection deg="115" code="ESE" name="East-southeast"></windDirection><windSpeed mps="6.62" name="Moderate breeze"></windSpeed><temperature day="12.79" min="11.07" max="12.79" night="12.12" eve="11.65" morn="11.07"></temperature><pressure unit="hPa" value="1030.19"></pressure><humidity value="0" unit="%"></humidity><clouds value="overcast clouds" all="100" unit="%"></clouds></time><time day="2016-03-22"><symbol number="501" name="moderate rain" var="10d"></symbol><precipitation value="11.99" type="rain"></precipitation><windDirection deg="84" code="E" name="East"></windDirection><windSpeed mps="6.84" name="Moderate breeze"></windSpeed><temperature day="15.99" min="11.54" max="15.99" night="11.54" eve="12.55" morn="13.37"></temperature><pressure unit="hPa" value="1023.35"></pressure><humidity value="0" unit="%"></humidity><clouds value="overcast clouds" all="97" unit="%"></clouds></time><time day="2016-03-23"><symbol number="501" name="moderate rain" var="10d"></symbol><precipitation value="10.75" type="rain"></precipitation><windDirection deg="18" code="NNE" name="North-northeast"></windDirection><windSpeed mps="5.51" name="Moderate breeze"></windSpeed><temperature day="11.39" min="8.22" max="11.39" night="8.22" eve="9.62" morn="10.57"></temperature><pressure unit="hPa" value="1029.89"></pressure><humidity value="0" unit="%"></humidity><clouds value="overcast clouds" all="97" unit="%"></clouds></time><time day="2016-03-24"><symbol number="500" name="light rain" var="10d"></symbol><precipitation value="1.67" type="rain"></precipitation><windDirection deg="25" code="NNE" name="North-northeast"></windDirection><windSpeed mps="7.47" name="Moderate breeze"></windSpeed><temperature day="13.35" min="6.82" max="13.35" night="6.82" eve="7.55" morn="8.62"></temperature><pressure unit="hPa" value="1036.05"></pressure><humidity value="0" unit="%"></humidity><clouds value="few clouds" all="24" unit="%"></clouds></time></forecast></weatherdata>"""
 		val page = try {
 			import jadecrawler.net.HTTPUtil
 			val url = ("http://%s/data/2.5/forecast/daily?q=%s&mode=xml&units=metric" + 
