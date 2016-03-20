d09e89ec766d2c607639371538bf5e6acfcd3582
Jade Shan <evokeralucard@gmail.com>
2016-03-20 23:24:11 +0800

    update

--- crawler-logic/src/test/scala/website/OpenWeatherTest.scala
+++ crawler-logic/src/test/scala/website/OpenWeatherTest.scala
@@ -15,9 +15,11 @@ import java.util.Properties
 class OpenWeatherTest extends FunSuite with Logging {
 
 	test("Test-16days") {
-		val result = OpenWeatherCrawler.get16dayByCityName("Shanghai,cn", "GMT+8", "b1b15e88fa797225412429c1c50c122a")
+		val result = OpenWeatherCrawler.get16dayByCityName("Shanghai,cn", "GMT+8", 
+			"b1b15e88fa797225412429c1c50c122a")
 		logDebug("{}", result)
 		val str = OpenWeatherParser.formatConkyWeather("Asia/Shanghai", "zh_CN", result)
 		logDebug("\n{}", str)
 	}
+
 }
